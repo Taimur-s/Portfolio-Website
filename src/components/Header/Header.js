9e24849d61d97c770a23ac83223381a70b2d57da
Taimur-s <94656455+Taimur-s@users.noreply.github.com>
2023-02-23 01:00:39 +0500

    HeaderFInal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,18 +3,18 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillMobile, AiFillPhone } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
   <Container>
     <Div1>
       <Link legacyBehavior href={'/'}>
-        <a style={{ display: 'flex', alignItems:'center', color:'white'}}>
-          <DiCssdeck size='3rem'/> <span>Portfolio</span>
+        <a style={{ display: 'flex', alignItems:'center', color:'white', marginBottom:'20px'}}>
+          <DiCssdeck size='2rem'/> <Span>Portfolio</Span>
           </a>
       </Link>
     </Div1>
-    <Div2>
+    <Div2 style={{marginTop:'2px'}}>
       <li>
         <Link href={'#projects'}>
           <NavLink>Projects</NavLink>
@@ -31,7 +31,7 @@ const Header = () =>  (
         </Link>
       </li>
     </Div2>
-    <Div3>
+    <Div3 style={{marginTop:'2px'}}>
       <SocialIcons  href={'https://github.com/Taimur-s/'}>
         <AiFillGithub size= {'3rem'}/>
       </SocialIcons>
