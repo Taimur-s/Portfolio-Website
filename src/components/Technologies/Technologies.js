f9ff9a4213532b94bd040d7a0b84b621c3b9272f
Taimur-s <94656455+Taimur-s@users.noreply.github.com>
2023-02-23 03:54:33 +0500

    techFInal

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,51 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiAws, DiCss3Full, DiDatabase, DiDocker, DiFirebase, DiMongodb, DiNodejs, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id='tech'>
+    <SectionDivider/>
+    <br/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked wiath a rage of technologies in thw WebDev World.
+      From FrontEnd to BackEnd
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size='3rem'/>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiDatabase size='3rem'/>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            Node and Databases 
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiCss3Full size='3rem'/>
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            Tools like Adobe XD and Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+    </List>
+  </Section>
 );
 
 export default Technologies;
