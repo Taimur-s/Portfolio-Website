4ac4400173ada5877dbe253678448a827ba72f64
Taimur-s <94656455+Taimur-s@users.noreply.github.com>
2023-02-23 05:28:35 +0500

    commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,34 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href='tel:0092-331-401-0886'>0092-331-401-0886</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href='mailto:tamur.hashmi@gmail.com'>tamur.hashmi@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href={'https://github.com/Taimur-s/'}>
+            <AiFillGithub size={'3rem'} />
+          </SocialIcons>
+          <SocialIcons href={'https://www.linkedin.com/in/taimursh/'}>
+            <AiFillLinkedin size={'3rem'} />
+          </SocialIcons>
+          <SocialIcons href={'https://wa.me/message/6UBWHEFI5IS7I1'}>
+            <AiFillPhone size={'3rem'} />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
