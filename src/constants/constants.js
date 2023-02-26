86b251865c0a45053f1df034e89ae8a3f3aaa035
Taimur-s <94656455+Taimur-s@users.noreply.github.com>
2023-02-26 08:40:20 +0500

    commit

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,7 +10,7 @@ export const projects = [
   },
   {
     title: 'E-Modern UI/UX GPT-3',
-    description:"The application is developed using React functional components, with the design using Tailwind CSS flex and grid, following the CSS BEM model. The interface includes gentle animations and intricate gradients. The media queries are strategically positioned for optimal responsiveness across a wide range of devices.",
+    description:"The application is developed using React, Next, with the design using CSS flex and grid, following the CSS BEM model. The interface includes gentle animations and intricate gradients. The media queries positioned for optimal responsiveness across a wide range of devices.",
     image: '/images/2.png',
     tags: ['React','Next', 'JavaScript'],
     source: 'https://github.com/Taimur-s/gpt3-uiux',
@@ -19,7 +19,7 @@ export const projects = [
   },
   {
     title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+    description: "This is a code repository for the corresponding , we're going to build and deploy a React Video Chat Application using WebRTC.",
       image: '/images/3.jpg',
       tags: ['React', 'WebRTC'],
     source: 'https://google.com',
@@ -28,7 +28,7 @@ export const projects = [
   },
   {
     title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+    description: "This is a code repository for the . In this video, we will create a full Realtime Chat Application",
     image: '/images/4.jpg',
     tags: ['React', 'ChatEngine', 'Firebase'],
     source: 'https://google.com',
